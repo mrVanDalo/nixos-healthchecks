e6201cfb91d9fd39b3797b3d3680dbc303cd904a
Ingolf Wagner <contact@ingolf-wagner.de>
2024-10-14 20:21:25 +0900

    :recycle: extracted pretty printer struct

--- pkgs/script-exec/examples/success.sh
+++ pkgs/script-exec/examples/success.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+sleep 1
 echo "all good"
 echo "this should never be printed"
 
--- pkgs/script-exec/src/main.rs
+++ pkgs/script-exec/src/main.rs
@@ -4,7 +4,7 @@ use log::debug;
 use std::io::{self, Write};
 use std::path::Path;
 use std::process::{exit, Command};
-use std::time::Instant;
+use std::time::{Duration, Instant};
 
 /// Run healthcheck commands and make them look pretty.
 #[derive(Parser, Debug)]
@@ -26,37 +26,74 @@ struct Args {
     path: String,
 }
 
-fn exit_code_visualization(use_emoji: bool) -> (&'static str, &'static str, &'static str) {
-    if use_emoji {
-        ("⏳", "✅", "❌")
-    } else {
-        ("[Wait]", "[ OK ]", "[Fail]")
+/// PrettyPrinter
+/// containing all the information needed to print user-friendly output.
+struct PrettyPrinter {
+    /// use emojis in printout or not
+    use_emoji: bool,
+
+    /// title of the execution
+    title: String,
+}
+
+impl PrettyPrinter {
+    fn new(title: String, use_emoji: bool) -> Self {
+        Self { use_emoji, title }
+    }
+
+    /// decision function on what to print
+    fn exit_code_visualization(
+        &self,
+        use_emoji: bool,
+    ) -> (&'static str, &'static str, &'static str) {
+        if use_emoji {
+            ("⏳", "✅", "❌")
+        } else {
+            ("[Wait]", "[ OK ]", "[Fail]")
+        }
+    }
+
+    /// print waiting line
+    fn waiting(&self) {
+        let (wait, _, _) = self.exit_code_visualization(self.use_emoji);
+        print!("{} {}", wait, self.title);
+        io::stdout().flush().unwrap();
+    }
+
+    /// print success line
+    fn success(&self, duration: Duration) {
+        let (_, ok, _) = self.exit_code_visualization(self.use_emoji);
+        print!("\r\x1B[2K"); // \x1B[2K clears the entire line
+        println!("{} {} [{:.2?}s]", ok, self.title, duration.as_secs_f64());
+    }
+
+    /// print failure line
+    fn failure(&self, duration: Duration) {
+        let (_, _, fail) = self.exit_code_visualization(self.use_emoji);
+        print!("\r\x1B[2K"); // \x1B[2K clears the entire line
+        println!("{} {} [{:.2?}s]", fail, self.title, duration.as_secs_f64());
     }
 }
 
-fn run_script(script: &str, title: &str, use_emoji: bool) {
-    let (wait, ok, fail) = exit_code_visualization(use_emoji);
-
+fn run_script(script: &str, pretty_printer: PrettyPrinter) {
     if !Path::new(script).exists() {
-        println!("{} {}", fail, title);
+        pretty_printer.failure(Duration::new(0, 0));
         eprintln!("{} does not exist", script);
         exit(1);
     }
 
-    print!("{} {}", wait, title);
-    io::stdout().flush().unwrap();
+    pretty_printer.waiting();
 
     let start = Instant::now();
     let result = Command::new(script)
         .output()
         .expect("Failed to execute script");
     let duration = start.elapsed();
-    print!("\r\x1B[2K"); // \x1B[2K clears the entire line
 
     if result.status.success() {
-        println!("{} {} [{:.2?}s]", ok, title, duration.as_secs_f64());
+        pretty_printer.success(duration);
     } else {
-        println!("{} {} [{:.2?}s]", fail, title, duration.as_secs_f64());
+        pretty_printer.failure(duration);
         println!("Output:\n{}", String::from_utf8_lossy(&result.stdout));
         println!("Error:\n{}", String::from_utf8_lossy(&result.stderr));
     }
@@ -73,9 +110,7 @@ fn main() {
     debug!("Script path: {}", args.path);
     debug!("Emojis enabled : {}", args.emoji);
 
-    run_script(
-        args.path.clone().as_str(),
-        args.title.unwrap_or(args.path).as_str(),
-        args.emoji,
-    );
+    let pretty_printer = PrettyPrinter::new(args.title.unwrap_or(args.path.clone()), args.emoji);
+
+    run_script(args.path.as_str(), pretty_printer);
 }
