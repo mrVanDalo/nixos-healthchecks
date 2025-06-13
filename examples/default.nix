ce5cc5ec8f1a23e17573f838bc27ec2e3b8af800
Ingolf Wagner <contact@ingolf-wagner.de>
2025-06-13 20:55:32 +0200

    :memo: update example.gif

--- examples/default.nix
+++ examples/default.nix
@@ -2,6 +2,7 @@
 
   # public domain/ip address to test for closed ports
   healthchecks.closed.public.host = "example.com";
+  healthchecks.config.max-jobs = 2; # to see something on the example.gif
 
   imports = [
     ./gitea.nix
